b03677a6a1c51d4bcae2abd406d7d5499b6e6b8c
pbrendin <pbrendin@gmail.com>
2022-02-27 23:14:51 -0500

    Adding all edited files

--- src/VendingMachine.jsx
+++ src/VendingMachine.jsx
@@ -1,61 +1,64 @@
 import { Collapse } from "bootstrap";
 import React, { Component } from "react";
-
+import download from "downloadjs";
 export default class VendingMachine extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pageTitle: "Products",
-      productCount: 2,
-      changeAmount: 0,
-      moneyInserted: 0,
-      numberOfDollars: 0,
-      numberOfFiveDollars: 0,
-      numberOfQuarters: 0,
-      numberOfDimes: 0,
-      numberOfNickles: 0,
-      valueOfDollar: 1,
-      valueOfFiveDollars: 5,
-      valueOfQuarter: 0.25,
-      valueOfDime: 0.1,
-      valueOfNickel: 0.05,
-      billFiveQuantity: 0,
-      billOneQuantity: 0,
-      coinQuarterQuantity: 0,
-      coinDimeQuantity: 0,
-      coinNickelQuantity: 0,
+      vend_details: {
+        pageTitle: "Products",
+        purchasedProduct: "",
+        productCount: 0,
+        changeAmount: 0,
+        moneyInserted: 0,
+        numberOfDollars: 0,
+        numberOfFiveDollars: 0,
+        numberOfQuarters: 0,
+        numberOfDimes: 0,
+        numberOfNickles: 0,
+        valueOfDollar: 1,
+        valueOfFiveDollars: 5,
+        valueOfQuarter: 0.25,
+        valueOfDime: 0.1,
+        valueOfNickel: 0.05,
+        billFiveQuantity: 0,
+        billOneQuantity: 0,
+        coinQuarterQuantity: 0,
+        coinDimeQuantity: 0,
+        coinNickelQuantity: 0,
+      },
       productName: [
         {
+          id: 1,
           name: "Fizz",
           desc: "An effervescent fruity experience with hints of grape and coriander.",
           price: 1,
           amount: { amt_remain: 12, max_amount: 100 },
-          photo:
-            "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1276922850573292666/jpeg-600/G16.jpg",
+          pic: "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1276922850573292666/jpeg-600/G16.jpg",
         },
         {
+          id: 2,
           name: "Pop",
           desc: "An explosion of flavor that will knock your socks off!",
           price: 1,
           amount: { amt_remain: 25, max_amount: 200 },
-          photo:
-            "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1273408777629996108/jpeg-600/G13.jpg",
+          pic: "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1273408777629996108/jpeg-600/G13.jpg",
         },
         {
+          id: 3,
           name: "Cola",
           desc: "A basic no nonsense cola that is the perfect pick me up for any occasion.",
           price: 1,
           amount: { amt_remain: 25, max_amount: 100 },
-          photo:
-            "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1273215019533735338/jpeg-600/G03.jpg",
+          pic: "https://atlas-content1-cdn.pixelsquid.com/assets_v2/127/1273215019533735338/jpeg-600/G03.jpg",
         },
         {
+          id: 4,
           name: "Mega Pop",
           desc: "Not for the faint of heart.  So flavorful and so invigorating, it should probably be illegal.",
           price: 1,
           amount: { amt_remain: 25, max_amount: 50 },
-          photo:
-            "https://t3.ftcdn.net/jpg/00/66/97/22/360_F_66972273_DZ2iAaBiYdZ9pfwhiPmBRvjf7LmlYfKc.jpg",
+          pic: "https://t3.ftcdn.net/jpg/00/66/97/22/360_F_66972273_DZ2iAaBiYdZ9pfwhiPmBRvjf7LmlYfKc.jpg",
         },
       ],
     };
@@ -63,7 +66,9 @@ export default class VendingMachine extends Component {
   render() {
     return (
       <div>
-        <h4 className="border-bottom m-1 p-1">{this.state.pageTitle}</h4>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.vend_details.pageTitle}
+        </h4>
         <div className="card-group">{this.getProductRow()}</div>
         <div className="row row-cols-3 align-items-start">
           <div className="col-2">
@@ -90,7 +95,7 @@ export default class VendingMachine extends Component {
                 -
               </button>
               <span className="badge bg-secondary small-badge mx-2">
-                {this.state.billFiveQuantity}
+                {this.state.vend_details.billFiveQuantity}
               </span>
             </div>
           </div>
@@ -102,13 +107,12 @@ export default class VendingMachine extends Component {
                   You have Entered the following amount:
                 </h5>
                 <p className="card-text float-end">
-                  {this.state.moneyInserted.toFixed(2)}
+                  {this.state.vend_details.moneyInserted.toFixed(2)}
                 </p>
               </div>
               <div className="card-footer">
-                <span className="float-start">Change:</span>
-                <span className="float-end">
-                  {this.state.changeAmount.toFixed(2)}
+                <span className="float-start">
+                  {this.state.vend_details.purchasedProduct}
                 </span>
               </div>
             </div>
@@ -139,7 +143,7 @@ export default class VendingMachine extends Component {
                 -
               </button>
               <span className="badge bg-secondary small-badge mx-2">
-                {this.state.billOneQuantity}
+                {this.state.vend_details.billOneQuantity}
               </span>
             </div>
           </div>
@@ -169,7 +173,7 @@ export default class VendingMachine extends Component {
                 -
               </button>
               <span className="badge bg-secondary small-badge mx-2">
-                {this.state.coinQuarterQuantity}
+                {this.state.vend_details.coinQuarterQuantity}
               </span>
             </div>
           </div>
@@ -199,7 +203,7 @@ export default class VendingMachine extends Component {
                 -
               </button>
               <span className="badge bg-secondary small-badge mx-2">
-                {this.state.coinDimeQuantity}
+                {this.state.vend_details.coinDimeQuantity}
               </span>
             </div>
           </div>
@@ -229,7 +233,7 @@ export default class VendingMachine extends Component {
                 -
               </button>
               <span className="badge bg-secondary small-badge mx-2">
-                {this.state.coinNickelQuantity}
+                {this.state.vend_details.coinNickelQuantity}
               </span>
             </div>
           </div>
@@ -238,9 +242,12 @@ export default class VendingMachine extends Component {
     );
   }
 
-  componentDidMount() {
+  componentDidMount = async () => {
     console.log("componentDidMount");
-  }
+    let response = await fetch("http://localhost:5001/", { method: "GET" });
+    let prods = await response.json();
+    //console.log(prods);
+  };
 
   componentDidUpdate(prevProps, prevState) {
     console.log(
@@ -256,7 +263,7 @@ export default class VendingMachine extends Component {
   }
 
   componentWillUnmount() {
-    console.log("componentWillUnmount - Vending Machine");
+    //onsole.log("componentWillUnmount - Vending Machine");
   }
 
   componentDidCatch(error, info) {
@@ -267,13 +274,6 @@ export default class VendingMachine extends Component {
       info
     )}`;
   }
-
-  //Executes when a user clicks the button
-  // this can be used for adding to cart
-  onRefreshClick = () => {
-    this.setState({ productCount: 7 });
-    //console.log("Refresh Clicked");
-  };
 
   getDescription = (desc) => {
     return desc ? (
@@ -289,11 +289,7 @@ export default class VendingMachine extends Component {
     return this.state.productName.map((prod) => {
       return (
         <div className="card">
-          <img
-            src={prod.photo}
-            className="card-img-top"
-            alt="card-group-image"
-          />
+          <img src={prod.pic} className="card-img-top" alt="card-group-image" />
           <div className="card-body">
             <h5 className="card-title">
               {prod.name}
@@ -338,77 +334,118 @@ export default class VendingMachine extends Component {
   };
 
   handleIncrement = (currency) => {
-    if (currency == 5 && this.state.moneyInserted < 10) {
-      this.setState({ billFiveQuantity: this.state.billFiveQuantity + 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted + this.state.valueOfFiveDollars,
-      });
-    } else if (currency == 1 && this.state.moneyInserted < 10) {
-      this.setState({ billOneQuantity: this.state.billOneQuantity + 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted + this.state.valueOfDollar,
-      });
-    } else if (currency == 0.25 && this.state.moneyInserted < 10) {
-      this.setState({
-        coinQuarterQuantity: this.state.coinQuarterQuantity + 1,
-      });
-      this.setState({
-        moneyInserted: this.state.moneyInserted + this.state.valueOfQuarter,
-      });
-    } else if (currency == 0.1 && this.state.moneyInserted < 10) {
-      this.setState({ coinDimeQuantity: this.state.coinDimeQuantity + 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted + this.state.valueOfDime,
-      });
-    } else if (currency == 0.05 && this.state.moneyInserted < 10) {
-      this.setState({ coinNickelQuantity: this.state.coinNickelQuantity + 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted + this.state.valueOfNickel,
-      });
+    let v_detail = { ...this.state.vend_details };
+    if (
+      currency === 5 &&
+      v_detail.moneyInserted < 10 &&
+      v_detail.moneyInserted + v_detail.valueOfFiveDollars <= 10
+    ) {
+      v_detail.billFiveQuantity++;
+      v_detail.moneyInserted += v_detail.valueOfFiveDollars;
+      console.log("5 was added");
+    } else if (
+      currency === 1 &&
+      v_detail.moneyInserted < 10 &&
+      v_detail.moneyInserted + v_detail.valueOfDollar <= 10
+    ) {
+      v_detail.billOneQuantity++;
+      v_detail.moneyInserted += v_detail.valueOfDollar;
+    } else if (
+      currency === 0.25 &&
+      v_detail.moneyInserted < 10 &&
+      v_detail.moneyInserted + v_detail.valueOfQuarter <= 10
+    ) {
+      v_detail.coinQuarterQuantity++;
+      v_detail.moneyInserted += v_detail.valueOfQuarter;
+    } else if (currency === 0.1 && v_detail.moneyInserted <= 10) {
+      v_detail.coinDimeQuantity++;
+      v_detail.moneyInserted += v_detail.valueOfDime;
+    } else if (currency === 0.05 && v_detail.moneyInserted <= 10) {
+      v_detail.coinNickelQuantity++;
+      v_detail.moneyInserted += v_detail.valueOfNickel;
     }
+
+    this.setState({ vend_details: v_detail });
   };
 
   handleDecrement = (currency) => {
-    if (currency == 5 && this.state.billFiveQuantity > 0) {
-      this.setState({ billFiveQuantity: this.state.billFiveQuantity - 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted - this.state.valueOfFiveDollars,
-      });
-    } else if (currency == 1 && this.state.billOneQuantity > 0) {
-      this.setState({ billOneQuantity: this.state.billOneQuantity - 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted - this.state.valueOfDollar,
-      });
-    } else if (currency == 0.25 && this.state.coinQuarterQuantity > 0) {
-      this.setState({
-        coinQuarterQuantity: this.state.coinQuarterQuantity - 1,
-      });
-      this.setState({
-        moneyInserted: this.state.moneyInserted - this.state.valueOfQuarter,
-      });
-    } else if (currency == 0.1 && this.state.coinDimeQuantity > 0) {
-      this.setState({ coinDimeQuantity: this.state.coinDimeQuantity - 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted - this.state.valueOfDime,
-      });
-    } else if (currency == 0.05 && this.state.coinNickelQuantity > 0) {
-      this.setState({ coinNickelQuantity: this.state.coinNickelQuantity - 1 });
-      this.setState({
-        moneyInserted: this.state.moneyInserted - this.state.valueOfNickel,
-      });
+    let v_detail = { ...this.state.vend_details };
+
+    if (
+      currency === 5 &&
+      v_detail.billFiveQuantity > 0 &&
+      v_detail.moneyInserted - v_detail.valueOfFiveDollars >= 0
+    ) {
+      v_detail.billFiveQuantity--;
+      v_detail.moneyInserted -= v_detail.valueOfFiveDollars;
+    } else if (
+      currency === 1 &&
+      v_detail.billOneQuantity > 0 &&
+      v_detail.moneyInserted - v_detail.valueOfDollar >= 0
+    ) {
+      v_detail.billOneQuantity--;
+      v_detail.moneyInserted -= v_detail.valueOfDollar;
+    } else if (
+      currency === 0.25 &&
+      v_detail.coinQuarterQuantity > 0 &&
+      v_detail.moneyInserted - v_detail.valueOfQuarter >= 0
+    ) {
+      v_detail.coinQuarterQuantity--;
+      v_detail.moneyInserted -= v_detail.valueOfQuarter;
+    } else if (
+      currency === 0.1 &&
+      v_detail.coinDimeQuantity > 0 &&
+      v_detail.moneyInserted - v_detail.valueOfDime >= 0
+    ) {
+      v_detail.coinDimeQuantity--;
+      v_detail.moneyInserted -= v_detail.valueOfDime;
+    } else if (
+      currency === 0.05 &&
+      v_detail.coinNickelQuantity > 0 &&
+      v_detail.moneyInserted - v_detail.valueOfNickel >= 0
+    ) {
+      v_detail.coinNickelQuantity--;
+      v_detail.moneyInserted -= v_detail.valueOfNickel;
     }
+    this.setState({ vend_details: v_detail });
   };
 
-  buyProduct = (prod) => {
-    if (this.state.moneyInserted >= prod.price && prod.amount.amt_remain > 0) {
-      //console.log("buyProduct has been called");
+  buyProduct = async (prod) => {
+    //console.log("entered buyProduct");
+    if (
+      this.state.vend_details.moneyInserted >= prod.price &&
+      prod.amount.amt_remain > 0
+    ) {
+      console.log("buyProduct has been called");
+      let res = await fetch(`http://localhost:5001/products/${prod.name}`, {
+        method: "GET",
+      });
+
+      let blob = await res.blob();
+      download(blob, prod.name + ".json");
+
+      let v_detail = { ...this.state.vend_details };
       let allProducts = [...this.state.productName];
       let index = allProducts.indexOf(prod);
 
       allProducts[index].amount.amt_remain--;
+      v_detail.purchasedProduct =
+        "You have purchased " + allProducts[index].name;
+      v_detail.moneyInserted--;
+      v_detail.billFiveQuantity = 0;
+      v_detail.billOneQuantity = 0;
+      v_detail.coinDimeQuantity = 0;
+      v_detail.coinQuarterQuantity = 0;
+      v_detail.coinNickelQuantity = 0;
 
-      this.setState({ moneyInserted: this.state.moneyInserted - 1 });
+      this.setState({ vend_details: v_detail });
       this.setState({ productName: allProducts });
+    } else if (this.state.vend_details.moneyInserted < prod.price) {
+      this.state.vend_details.purchasedProduct = "Please insert $" + prod.price;
+      console.log(this.state.vend_details.purchasedProduct);
+      this.setState({
+        purchasedProduct: this.state.vend_details.purchasedProduct,
+      });
     }
   };
 }
