5d4be3b834a76b6ac3ca3dcd533abcdddfea9cb3
pbrendin <pbrendin@gmail.com>
2022-02-28 20:48:54 -0500

    Add files via upload

--- nodejs/src/controller/file.controller.js
+++ nodejs/src/controller/file.controller.js
@@ -1,8 +1,9 @@
+const path = require("path");
 const download = (req, res) => {
   console.log("entered download");
   const fileName = req.params.name + ".json";
-  const directoryPath = "C:/Users/Brendin/express-node/files/";
-  console.log(fileName);
+  const directoryPath = path.join(__dirname, "../../files/");
+  console.log("Files path is: ", directoryPath);
   res.download(directoryPath + fileName, fileName, (err) => {
     if (err) {
       res.status(500).send({
--- nodejs/src/routes/index.js
+++ nodejs/src/routes/index.js
@@ -2,15 +2,23 @@ const prod_data = require("../../files/vend_details.json");
 const express = require("express");
 const app = express();
 const controller = require("../controller/file.controller");
+var vend = JSON.stringify(prod_data);
+var parsed = JSON.parse(vend);
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
-app.get("/", (req, res) => {
+app.get("/getDetails", (req, res) => {
   //console.log(prod_data);
-  res.json(prod_data);
+  res.json(parsed.vend_details);
+  //res.json(prod_data);
+  //res.json(parsed.productName);
+});
+
+app.get("/getProducts", (req, res) => {
+  res.json(parsed.productName);
 });
 
 app.get("/products/:name", controller.download);
